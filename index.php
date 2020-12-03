558730dd69a7c78b016adf88c32242b6bd8d6fb1
DHANYA1997 <dhanyavarghese00@gmail.com>
2020-12-03 16:46:04 +0530

    background changed

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Login</a>
+            <a class="nav-link" href="index.php">Login</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
